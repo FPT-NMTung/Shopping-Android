fa1133231512b22e599275531af3e508ebc46aee
NMTung <nmtung.study@gmail.com>
2022-01-13 13:44:31 +0700

    Done Navigation

--- app/src/main/java/fu/prm391/sampl/project/Home.java
+++ app/src/main/java/fu/prm391/sampl/project/Home.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link home#newInstance} factory method to
+ * Use the {@link Home#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class home extends Fragment {
+public class Home extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class home extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public home() {
+    public Home() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class home extends Fragment {
      * @return A new instance of fragment home.
      */
     // TODO: Rename and change types and number of parameters
-    public static home newInstance(String param1, String param2) {
-        home fragment = new home();
+    public static Home newInstance(String param1, String param2) {
+        Home fragment = new Home();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
--- app/src/main/java/fu/prm391/sampl/project/Setting.java
+++ app/src/main/java/fu/prm391/sampl/project/Setting.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link setting#newInstance} factory method to
+ * Use the {@link Setting#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class setting extends Fragment {
+public class Setting extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class setting extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public setting() {
+    public Setting() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class setting extends Fragment {
      * @return A new instance of fragment setting.
      */
     // TODO: Rename and change types and number of parameters
-    public static setting newInstance(String param1, String param2) {
-        setting fragment = new setting();
+    public static Setting newInstance(String param1, String param2) {
+        Setting fragment = new Setting();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
