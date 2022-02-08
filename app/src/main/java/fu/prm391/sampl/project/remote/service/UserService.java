d13cf33a85d78ea282f9b3d264b83304d4cd15b6
Trung Nguyen <nguyenht65.se@gmail.com>
2022-02-08 16:53:53 +0700

    added verify email method

--- app/src/main/java/fu/prm391/sampl/project/remote/service/UserService.java
+++ app/src/main/java/fu/prm391/sampl/project/remote/service/UserService.java
@@ -6,8 +6,11 @@ import fu.prm391.sampl.project.model.user.LoginRequest;
 import fu.prm391.sampl.project.model.user.LoginResponse;
 import fu.prm391.sampl.project.model.user.RegisterRequest;
 import fu.prm391.sampl.project.model.user.RegisterResponse;
+import fu.prm391.sampl.project.model.user.VerifyEmailRequest;
+import fu.prm391.sampl.project.model.user.VerifyEmailResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 
 public interface UserService {
@@ -20,4 +23,7 @@ public interface UserService {
 
     @POST("user/send-email-forgot-password")
     Call<ForgotPassResponse> sendForgotPass(@Body ForgotPassRequest forgotPassRequest);
+
+    @PATCH("user/reset-password")
+    Call<VerifyEmailResponse> verifyEmail(@Body VerifyEmailRequest verifyEmailRequest);
 }
