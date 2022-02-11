f0e7e510bc9daf2409542666effb2a0d71a667a3
NMTung <nmtung.study@gmail.com>
2022-02-11 17:28:08 +0700

    Add layout shipping address

---
+++ app/src/main/java/fu/prm391/sampl/project/ProfileShippingAddress.java
@@ -0,0 +1,14 @@
+package fu.prm391.sampl.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileShippingAddress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_shipping_address);
+    }
+}
