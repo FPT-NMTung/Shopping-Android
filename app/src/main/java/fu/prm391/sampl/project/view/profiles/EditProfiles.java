2673ae828a7e9ffae1b6f3e21f6b78ebe7f82571
abcmonboi <95062907+abcmonboi@users.noreply.github.com>
2022-02-16 07:55:30 +0700

    check fill temp

--- app/src/main/java/fu/prm391/sampl/project/view/profiles/EditProfiles.java
+++ app/src/main/java/fu/prm391/sampl/project/view/profiles/EditProfiles.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -19,15 +20,16 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import fu.prm391.sampl.project.R;
 import fu.prm391.sampl.project.view.MainActivity;
+import fu.prm391.sampl.project.view.account.Login;
 import fu.prm391.sampl.project.view.fragment.Profiles;
 import fu.prm391.sampl.project.view.product.NewArrivalProduct;
 
 public class EditProfiles extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    ImageView btnBack,cover;
-    EditText firstName, lastName,emailAddress,phoneNumber;
+    ImageView btnBack, cover;
+    EditText firstName, lastName, emailAddress, phoneNumber;
     Spinner gender;
     FloatingActionButton fab;
-
+    Button btnSave;
 
 
     @Override
@@ -35,28 +37,64 @@ public class EditProfiles extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profiles);
         cover = findViewById(R.id.coverImg);
-        fab =findViewById(R.id.floatingActionButton);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ImagePicker.with(EditProfiles.this)
-
-//                        .crop()	    			//Crop image(Optional), Check Customization for more option
-//                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
-//                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
-                        .start();
-
-            }
-        });
+        fab = findViewById(R.id.floatingActionButton);
         btnBack = findViewById(R.id.imageViewBackEditProfile);
         firstName = findViewById(R.id.txtFirstName);
         lastName = findViewById(R.id.txtLastName);
         emailAddress = findViewById(R.id.editTextTextEmailAddress);
         phoneNumber = findViewById(R.id.editTextPhone);
+        btnSave = findViewById(R.id.btnSaveEditProfiles);
+
+        // button save action
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (firstName.getText().toString().matches("") || lastName.getText().toString().matches("") || emailAddress.toString().matches("") || phoneNumber.toString().matches("")) {
+                    if (firstName.getText().toString().matches("")) {
+                        Toast.makeText(EditProfiles.this, "Save Unsuccessful,Please Fill First Name", Toast.LENGTH_SHORT).show();
+                        return;
+                    } else if (lastName.getText().toString().matches("")) {
+                        Toast.makeText(EditProfiles.this, "Save Unsuccessful,Please Fill Last Name", Toast.LENGTH_SHORT).show();
+                        return;
+                    } else if (emailAddress.getText().toString().matches("")) {
+                        Toast.makeText(EditProfiles.this, "Save Unsuccessful,Please Fill Email Address", Toast.LENGTH_SHORT).show();
+                        return;
+                    } else if (phoneNumber.getText().toString().matches("")) {
+                        Toast.makeText(EditProfiles.this, "Save Unsuccessful,Please Fill Phone Number", Toast.LENGTH_SHORT).show();
+                        return;
+
+                    } else {
+                        Toast.makeText(EditProfiles.this, "Save Unsuccessful,Please Fill Your Information", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                } else {
+                    Toast.makeText(EditProfiles.this, "Save successful", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+
+
+            }
+        });
+
+        // upload image
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImagePicker.with(EditProfiles.this)
+
+                        .crop()                    //Crop image(Optional), Check Customization for more option
+                        .compress(1024)            //Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(150, 150)    //Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+
+            }
+        });
+
 
         // spinner gender
         gender = findViewById(R.id.spinnerGender);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,R.array.Gender, R.layout.color_spinner_layout);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.Gender, R.layout.color_spinner_layout);
         adapter.setDropDownViewResource(R.layout.spinner_dropdown_layout);
         gender.setAdapter(adapter);
         gender.setOnItemSelectedListener(this);
